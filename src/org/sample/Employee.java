8152ad1919c85d4c1452f306e9cfbbe995c474ae
sureshr91 <sureshrs.ram@gmail.com>
2022-06-24 09:47:44 +0530

    code added

---
+++ src/org/sample/Employee.java
@@ -0,0 +1,20 @@
+package org.sample;
+
+public class Employee {
+	
+	private void name() {
+		System.out.println("ragu");
+
+	}
+	private void id() {
+		System.out.println("6352645236");
+
+	}
+	private void age() {
+		System.out.println("25");
+
+	}
+	
+	
+
+}
